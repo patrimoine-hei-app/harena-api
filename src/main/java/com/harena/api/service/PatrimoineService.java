b9d19ac1ba9d9b7788760d1570541c18a1e4d088
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-07-29 01:01:19 +0200

    chore: get patrimoine

--- src/main/java/com/harena/api/service/PatrimoineService.java
+++ src/main/java/com/harena/api/service/PatrimoineService.java
@@ -3,7 +3,6 @@ package com.harena.api.service;
 import com.harena.api.repository.PatrimoineRepository;
 import java.io.File;
 import java.util.List;
-import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.hei.patrimoine.modele.Patrimoine;
@@ -17,11 +16,11 @@ public class PatrimoineService {
     return repository.getAllPaginatedPatrimoines(pageSize, page);
   }
 
-  public Optional<Patrimoine> getPatrimoineByName(String name) {
-    return repository.getPatrimoineByName(name);
-  }
-
   public void uploadPatrimoine(File file, String bucketKey) {
     repository.uploadPatrimoine(file, bucketKey);
+  }
+
+  public Patrimoine getPatrimoineByName(String name) {
+    return repository.getPatrimoineByName(name);
   }
 }
