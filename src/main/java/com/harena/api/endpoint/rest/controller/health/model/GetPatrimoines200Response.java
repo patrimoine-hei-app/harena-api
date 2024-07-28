175d9bda69a2b8e512d251ac52c4616abce4f2aa
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-07-28 23:41:39 +0200

    fix: fix get patrimoine

---
+++ src/main/java/com/harena/api/endpoint/rest/controller/health/model/GetPatrimoines200Response.java
@@ -0,0 +1,22 @@
+package com.harena.api.endpoint.rest.controller.health.model;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import school.hei.patrimoine.modele.Patrimoine;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetPatrimoines200Response {
+  private List<Patrimoine> data;
+  private int currentPage;
+  private int totalPages;
+  private long totalItems;
+
+  public GetPatrimoines200Response data(List<Patrimoine> data) {
+    this.data = data;
+    return this;
+  }
+}
