2db3e23c4ba0c95a5ea8ef73e7ca8c7877f1b7bf
Maharavo1 <hei.maharavo@gmail.com>
2024-07-28 07:17:37 -0700

    chore: implement PossessionService to update and create a possession

---
+++ src/main/java/com/harena/api/service/PossessionService.java
@@ -0,0 +1,15 @@
+package com.harena.api.service;
+import com.harena.api.repository.PossessionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.io.File;
+
+@Service
+@AllArgsConstructor
+public class PossessionService {
+    private final PossessionRepository possessionRepository;
+
+    public void uploadPossession(File file, String bucketKey) {
+        possessionRepository.uploadPossession(file, bucketKey);
+    }
+}
