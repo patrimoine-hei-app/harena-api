8c3af3ff6b988271513856e314c8a449a84bc238
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-07-28 09:21:04 +0200

    chore: PatrimoineService:: add method to save and update

--- src/main/java/com/harena/api/service/PatrimoineService.java
+++ src/main/java/com/harena/api/service/PatrimoineService.java
@@ -4,6 +4,8 @@ import com.harena.api.repository.PatrimoineRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.hei.patrimoine.modele.Patrimoine;
+
+import java.io.File;
 import java.util.List;
 
 @Service
@@ -13,5 +15,8 @@ public class PatrimoineService {
 
     public List<Patrimoine> getPaginatedPatrimoines(int page, int pageSize) {
         return repository.getAllPaginatedPatrimoines(pageSize, page);
+    }
+    public void uploadPatrimoine(File file, String bucketKey) {
+        repository.uploadPatrimoine(file, bucketKey);
     }
 }
