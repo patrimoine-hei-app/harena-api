5966924c5a2d65c1dbac713cdef720981e049165
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-07-28 09:14:25 +0200

    chore: PatrimoineService:: add  method to  receive list of patrimoine whith pagination

---
+++ src/main/java/com/harena/api/service/PatrimoineService.java
@@ -0,0 +1,17 @@
+package com.harena.api.service;
+
+import com.harena.api.repository.PatrimoineRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.patrimoine.modele.Patrimoine;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PatrimoineService {
+    private final PatrimoineRepository repository;
+
+    public List<Patrimoine> getPaginatedPatrimoines(int page, int pageSize) {
+        return repository.getAllPaginatedPatrimoines(pageSize, page);
+    }
+}
